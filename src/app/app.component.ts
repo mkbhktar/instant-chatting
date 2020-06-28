1d90a4ef1531a4f07c38352f725c5610a31a5245
Milind <mkbhuktar@gmail.com>
2020-06-28 15:18:48 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {InstantChatService} from './instant-chat.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'instant-chatting';
+  user: String;
+  room: String;
+  // tslint:disable-next-line:ban-types
+  messageText: String;
+  messageArray: Array<{user: String , message: String }> = [];
+  constructor(private instantChatservice: InstantChatService){
+    this.instantChatservice.newUserJoined()
+      .subscribe(data => this.messageArray.push(data));
+
+
+    this.instantChatservice.userLeftRoom()
+      .subscribe(data => this.messageArray.push(data));
+
+    this.instantChatservice.newMessageReceived()
+      .subscribe(data => this.messageArray.push(data));
+  }
+
+  join(){
+      this.instantChatservice.joinRoom({user: this.user, room: this.room});
+  }
+
+  leave(){
+    this.instantChatservice.leaveRoom({user: this.user, room: this.room});
+  }
+
+  sendMessage()
+  {
+    this.instantChatservice.sendMessage({user: this.user, room: this.room, message: this.messageText});
+  }
 }
